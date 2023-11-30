082555f419a7beeb6a6b3211692fd6d44dd0aecf
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-11-30 14:45:19 -0300

    feat (api): add the GET /list_user/:id route to search. This file defines a route to fetch users via a GET request to /list_user/:id. Request handling logic is delegated to the SearchUser.controller controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { CreateUserController } from "../controllers/createUserController/Create
 import { AuthUserController } from "../controllers/authUserController/AuthUser.controller";
 import { ListUserController } from "../controllers/listUserController/ListUser.controller";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { SearchUserController } from "../controllers/searchUserController/searchUser.controller";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const authUserController = new AuthUserController();
 const listUserController = new ListUserController();
+const searchUserController = new SearchUserController();
 
 router.post("/api/signUp", createUserController.handleSignUp);
 router.post("/api/signIn", authUserController.handleSignIn);
@@ -16,6 +18,11 @@ router.get(
   "/api/list_user",
   ensureAuthenticated,
   listUserController.handleList,
+);
+router.get(
+  "/api/list_user/:id",
+  ensureAuthenticated,
+  searchUserController.handleSearch,
 );
 
 export { router };
