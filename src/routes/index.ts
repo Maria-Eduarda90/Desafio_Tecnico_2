4e111dddbf8a2da1e0b22367db70fa581d3fc210
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-11-29 23:39:24 -0300

    feat(api): add GET /list_user route to list users. This file defines a route to list users via a GET request to /list_user. Request handling logic is delegated to the ListUser.controller controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/createUserController/CreateUser.controller";
 import { AuthUserController } from "../controllers/authUserController/AuthUser.controller";
+import { ListUserController } from "../controllers/listUserController/ListUser.controller";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const authUserController = new AuthUserController();
+const listUserController = new ListUserController();
 
 router.post("/api/signUp", createUserController.handleSignUp);
 router.post("/api/signIn", authUserController.handleSignIn);
+router.get(
+  "/api/list_user",
+  ensureAuthenticated,
+  listUserController.handleList,
+);
 
 export { router };
